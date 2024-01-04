9d1f8954a9ff69580b37943af9eed7f49c4934f6
Steven Schultz <104775272+SDSchultz@users.noreply.github.com>
2024-01-04 11:25:41 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-document.getElementById('generateButton').addEventListener('click', async function() {
-  try {
-    // Fetch moon phase data
-    const moonPhaseResponse = await fetch('https://api.ipgeolocation.io/astronomy?apiKey=9ce55516d5354e2f872c48da8fe47923');
-    const moonPhaseData = await moonPhaseResponse.json();
-    const moonPhaseElement = document.getElementById('moonPhase');
-    moonPhaseElement.innerHTML = 'Current Moon Phase: ' + moonPhaseData.moon_phase + ' (' + moonPhaseData.moon_illumination + ' illuminated)';
-  } catch (error) {
-    console.error('Error fetching moon phase:', error);
-    const moonPhaseElement = document.getElementById('moonPhase');
-    moonPhaseElement.innerHTML = 'Unable to fetch moon phase information.';
-  }
-
+document.getElementById('generateButton').addEventListener('click', function() {
   // Generate numbers based on the existing method
   let whiteBalls = [];
   for (let i = 0; i < 5; i++) {
@@ -23,20 +11,7 @@ document.getElementById('generateButton').addEventListener('click', async functi
   let result = document.getElementById('result');
   result.innerHTML = '<strong>' + whiteBalls.join(', ') + '</strong> Powerball: <strong>' + powerBall + '</strong>';
 
-  // Calculate astrological sign based on current date
-  const astrologicalSignElement = document.getElementById('astrologicalSign');
-  const currentDate = new Date();
-  const month = currentDate.getMonth() + 1;
-  const day = currentDate.getDate();
-
-  const astrologicalSigns = [
-    'Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini',
-    'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn'
-  ];
-
-  const astrologicalSignIndex = (day <= 21) ? (month - 1) : month;
-  const astrologicalSign = astrologicalSigns[astrologicalSignIndex];
-  if (astrologicalSignElement) {
-    astrologicalSignElement.innerHTML = 'Current Astrological Sign: ' + astrologicalSign;
-  }
+  // Placeholder for the PowerBall jackpot amount
+  const jackpotElement = document.getElementById('jackpot');
+  jackpotElement.innerHTML = 'Current Jackpot: $35 Million';
 });
